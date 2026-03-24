5335cc7cd9f73529d95e7dc0de50f2a8c6cb2ccf
Karthik Somayajula <75628388+karthik-com@users.noreply.github.com>
2026-03-24 21:46:24 +0530

    Add files via upload

---
+++ Day 4/day4project_rock_paper_scissors.py
@@ -0,0 +1,52 @@
+## Project - Rock, Paper, Scissors 
+
+
+# Solution - 
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+import random
+game_images = [rock, paper, scissors]
+user_choice = int(input("Enter your choice: 0 for rock, 1 for paper, 2 for scissors: "))
+if user_choice >= 0 and user_choice <= 2:
+    print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose: ")
+print(game_images[computer_choice])
+
+
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number. You lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose!")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif computer_choice == user_choice:
+    print("Draw!")
